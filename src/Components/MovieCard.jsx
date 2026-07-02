299a42acf1cbcb160abcbaf8718c8005e7bd300e
alimirzaeimajd <pe4090731@gmail.com>
2026-07-02 18:11:31 +0330

    style: update movie card

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { BLOG_POSTS } from "../blogs";
 import { slugify } from "../utils/slugify";
 
-export default function MovieCard({ image, title, genres, synopsis, imdbUrl, index }) {
+export default function MovieCard({ image, title, genres, length, rating, imdbUrl }) {
   const slug = slugify(title);
   const hasBlogPost = Boolean(BLOG_POSTS[slug]);
 
@@ -13,18 +13,26 @@ export default function MovieCard({ image, title, genres, synopsis, imdbUrl, ind
         <img className="movie-card__img" src={image} alt={title} />
       </div>
       <div className="movie-card__body">
-        <span className="movie-card__num">{String(index + 1).padStart(2, "0")}</span>
-        <span className="movie-card__genre">{genres}</span>
         <h2 className="movie-card__title">{title}</h2>
-        <hr className="movie-card__divider" />
-        <p className="movie-card__synopsis">{synopsis}</p>
+
+        {rating != null && (
+          <p className="movie-card__rating">
+            {rating}/10 <span className="movie-card__star">★</span>
+          </p>
+        )}
+
+        <div className="movie-card__meta">
+          <span className="movie-card__genre">{genres}</span>
+          {length && <span className="movie-card__length">{length}</span>}
+        </div>
+
         <div className="movie-card__actions">
           <a className="movie-card__imdb-btn" href={imdbUrl} target="_blank" rel="noreferrer">
-            IMDb ↗
+            IMDb
           </a>
           {hasBlogPost && (
             <Link className="movie-card__blog-btn" to={`/blog/${slug}`}>
-              Read the blog
+              Blog
             </Link>
           )}
         </div>
--- src/movies.js
+++ src/movies.js
@@ -8,6 +8,8 @@ export const MOVIES = [
     image: queenOfChessImg,
     title: "Queen of Chess (2026)",
     genres: "Documentary",
+    length: "N/A", // TODO: fill in real runtime
+    rating: 10,
     synopsis:
       "Hungarian chess prodigy Judit Polgár challenges champion Garry Kasparov and her controlling father over 15 years, breaking gender barriers to become the greatest female player ever.",
     imdbUrl: "https://www.imdb.com/title/tt39162904",
@@ -16,6 +18,8 @@ export const MOVIES = [
     image: fiveHundredDaysOfSummerImg,
     title: "500 Days of Summer (2009)",
     genres: "Romance/Comedy",
+    length: "95 min",
+    rating: 10,
     synopsis:
       "Tom revisits the approximate one year he shared with Summer, the girl he thought he could spend the rest of his life with. She, on the other hand, does not believe in relationships or boyfriends.",
     imdbUrl: "https://www.imdb.com/title/tt1022603",
@@ -24,15 +28,18 @@ export const MOVIES = [
     image: greatestHitsImg,
     title: "The Greatest Hits (2024)",
     genres: "Romance/Musical",
+    length: "94 min",
+    rating: 8,
     synopsis:
       "Harriet discovers certain songs can transport her back in time. While she relives the past through romantic memories with her former boyfriend, her time travelling interferes with a burgeoning new love interest in the present.",
     imdbUrl: "https://www.imdb.com/title/tt2015349",
   },
-
   {
     image: scottPilgrimVsTheWorldImg,
     title: "Scott Pilgrim vs. the World (2010)",
     genres: "Action/Comedy",
+    length: "112 min",
+    rating: 7,
     synopsis:
       "Scott Pilgrim meets Ramona and instantly falls in love with her. But when he meets one of her exes at a band competition, he realises that he has to deal with all seven of her exes to woo her.",
     imdbUrl: "https://www.imdb.com/title/tt0446029",
